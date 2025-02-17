c0e240345774ec2af6c41f14c6a60faa90a9aa4f
CEP-TARDA <eperaltas2324@politecnics.barcelona>
2025-02-17 18:46:26 +0100

    17/02/25 Detalles: - Iniciada creación del calendario. Falta centrado y formateado del estilo de las fechas. - Falta RecyclerView con mostrado de tareas.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,14 +35,15 @@ android {
     }
 }
 
-dependencies {
+    dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        implementation ("com.applandeo:material-calendar-view:1.9.0")
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+    }
