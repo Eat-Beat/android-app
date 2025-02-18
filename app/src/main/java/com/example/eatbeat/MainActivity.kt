2465c52036559a48481c29c1671cd021e22c17be
CEP-TARDA <eperaltas2324@politecnics.barcelona>
2025-02-18 20:20:18 +0100

    18/02/2025 | Detalles - En activity_contracts_calendar.xml introducido boton para cambiar a activity_contracts_list. - Creado y diseñado formato para la lista de activity_contracts_calendar.xml en forma de contract_calendar_list_cell.xml. - Creada y diseñada activity_contract_list.xml. - Creado y diseñado contract_format_cell.xml, que servirá mostrar los detalles. - Creado fragment_contract_details. Falta acabar de diseñar.

--- app/src/main/java/com/example/eatbeat/MainActivity.kt
+++ app/src/main/java/com/example/eatbeat/MainActivity.kt
@@ -3,18 +3,15 @@ package com.example.eatbeat
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.applandeo.materialcalendarview.CalendarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_contracts)
+        setContentView(R.layout.activity_contracts_calendar)
 
         val calendarView : CalendarView = findViewById(R.id.calendarView)
         calendarView.setCalendarDayLayout(R.layout.day_cell)
-
     }
 }
