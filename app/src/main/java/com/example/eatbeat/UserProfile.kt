9773fc6d8a969821bfc02bbe2c840dcc6d840203
CEP-TARDA <eperaltas2324@politecnics.barcelona>
2025-02-27 20:21:40 +0100

    26/02/2025 | Detalles - Hecho settings para cambiar idioma y cerrar sesión. Incluye   animación de entrada y salida. - Creadas activities y fragment para tener el codigo del funcionamiento.

--- app/src/main/java/com/example/eatbeat/UserProfile.kt
+++ app/src/main/java/com/example/eatbeat/UserProfile.kt
@@ -1,31 +1,45 @@
 package com.example.eatbeat
 
-import android.media.Image
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.FragmentContainerView
-import com.applandeo.materialcalendarview.CalendarView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class MainActivity : AppCompatActivity() {
+class UserProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_user_musician)
-
         val bottomSheet = findViewById<View>(R.id.profilesheet)
         val optionsButton = findViewById<ImageView>(R.id.settings_icon)
 
         BottomSheetBehavior.from(bottomSheet).apply {
-            peekHeight = 490
+            peekHeight = 330
             state = BottomSheetBehavior.STATE_COLLAPSED
         }
 
+        settingsClick(optionsButton)
+
+
+    }
+
+    private fun settingsClick(optionsButton: ImageView){
+        optionsButton.setOnClickListener{
+            val fragment = SettingsFrag()
+            val fadeIn = AnimationUtils.loadAnimation(this, R.anim.alpha_show_up)
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.optionsScreen, fragment)
+                .addToBackStack(null)
+                .commit()
+            val optionsScreen = findViewById<FragmentContainerView>(R.id.optionsScreen)
+
+            optionsScreen.startAnimation(fadeIn)
+            optionsScreen.visibility = View.VISIBLE
+        }
     }
 }
